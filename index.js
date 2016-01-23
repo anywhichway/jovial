3c213e6a9e988eacbf26d3587c807616d132dac6
syblackwell <syblackwell@anywhichway.com>
2016-01-23 11:05:15 -0500

    adjust Proxy include for server

--- index.js
+++ index.js
@@ -41,7 +41,10 @@
 		}
 	}
 	if(typeof(Proxy)==="undefined"  && typeof(require)==="function") {
-		require('chrome-proxy');
+		var ProxyConstructor = require('chrome-proxy');
+		if(this!==window) {
+			Proxy = Constructor;
+		}
 	}
 	Validator.prototype.bind = function(constructorOrObject,onerror,name) {
 		function validate(target,property,value,proxy,skipset,skiponerror,error) { 
